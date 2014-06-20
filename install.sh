82686fbeb0914e8359d6bece560a4806d2bc8063
Ron WL <ronwl@users.noreply.github.com>
2014-06-20 14:58:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ wget -N http://httpupdate.cPanel.net/latest
 sh latest
 
 # Install EPEL repo
-su -c 'rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+su -c 'rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm' > /var/log/cron.log 2>&1
 
 # Install useful monitoring tool
-yum install -y htop iotop iftop nethogs vnstat
+yum install -y htop iotop iftop nethogs vnstat > /var/log/cron.log 2>&1
 
 # Prevent "IP address have changed" error
 sed -i 's/cookieipvalidation=strict/cookieipvalidation=loose/g' /var/cpanel/cpanel.config
-/usr/local/cpanel/whostmgr/bin/whostmgr2 --updatetweaksettings
+/usr/local/cpanel/whostmgr/bin/whostmgr2 --updatetweaksettings > /var/log/cron.log 2>&1
 
 exit
